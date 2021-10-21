f7b06eac20f3d05df1fa8436ff64a92e8f829340
HappyGromper <mschiereck@verizon.net>
2021-10-20 23:14:58 -0400

    Add project files.

---
+++ Minecraft Book Maker/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace Minecraft_Book_Maker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.inputTextBox = new System.Windows.Forms.RichTextBox();
+            this.generateBtn = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.addTextBtn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.inputTextBox.Location = new System.Drawing.Point(14, 47);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(413, 425);
+            this.inputTextBox.TabIndex = 0;
+            this.inputTextBox.Text = "";
+            // 
+            // generateBtn
+            // 
+            this.generateBtn.Font = new System.Drawing.Font("Minecraft", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generateBtn.Location = new System.Drawing.Point(12, 478);
+            this.generateBtn.Name = "generateBtn";
+            this.generateBtn.Size = new System.Drawing.Size(271, 47);
+            this.generateBtn.TabIndex = 1;
+            this.generateBtn.Text = "Generate";
+            this.generateBtn.UseVisualStyleBackColor = true;
+            this.generateBtn.Click += new System.EventHandler(this.generateBtn_Click);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.outputTextBox.Font = new System.Drawing.Font("Minecraft", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputTextBox.Location = new System.Drawing.Point(506, 47);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(413, 425);
+            this.outputTextBox.TabIndex = 2;
+            this.outputTextBox.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Minecraft", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Input text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Minecraft", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(501, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(211, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Output text";
+            // 
+            // addTextBtn
+            // 
+            this.addTextBtn.Font = new System.Drawing.Font("Minecraft", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addTextBtn.Location = new System.Drawing.Point(289, 478);
+            this.addTextBtn.Name = "addTextBtn";
+            this.addTextBtn.Size = new System.Drawing.Size(271, 47);
+            this.addTextBtn.TabIndex = 5;
+            this.addTextBtn.Text = "Add Text";
+            this.addTextBtn.UseVisualStyleBackColor = true;
+            this.addTextBtn.Click += new System.EventHandler(this.addTextBtn_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Minecraft", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(719, 478);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 47);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Instructions";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Minecraft", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(827, 528);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 11);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Made by: Erou";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 543);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.addTextBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.generateBtn);
+            this.Controls.Add(this.inputTextBox);
+            this.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Minecraft Book Compiler";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox inputTextBox;
+        private System.Windows.Forms.Button generateBtn;
+        private System.Windows.Forms.RichTextBox outputTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button addTextBtn;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Minecraft Book Maker/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minecraft_Book_Maker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+
+        }
+
+        private string[] pages;
+
+        private int pageCount;
+
+        private void generateBtn_Click(object sender, EventArgs e)
+        {
+            pageCount = 1;
+            outputTextBox.Text = "";
+
+            string[] storyString = inputTextBox.Text.Split(' ');
+
+            outputTextBox.Text += "\tPage " + (1) + "\n\n";
+            string pageString = "";
+            
+            int extraCounter = 0;
+            for (int i = 0; i < storyString.Count(); i++)
+            {
+
+
+
+                pageString += storyString[i] + " ";
+
+                if (pageString.Length >= 265)
+                {
+
+                    outputTextBox.Text += pageString + "\n\n";
+
+                    pageString = "";
+                    outputTextBox.Text += "\n\n\tPage " + (++pageCount) + "\n\n";
+
+                    extraCounter = (i + 1);
+                }
+
+
+
+                //while (pageString.Length <= 180)
+                //{
+                //    pageString += storyString[i];
+                //}
+
+
+            }
+
+            for ( int i = 0;  extraCounter < storyString.Count(); extraCounter++)
+            {
+
+
+                outputTextBox.Text += storyString[extraCounter] + " ";
+
+            }
+
+
+            
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Add your text into the input box\nThen hit the generate button\nif you want to add more text use teh 'Add Text' button\niif you hit generate you will delete all text in the outputbox\n", "How to Use");
+
+        }
+
+        private void addTextBtn_Click(object sender, EventArgs e)
+        {
+            string[] storyString = inputTextBox.Text.Split(' ');
+
+            outputTextBox.Text += "\n\n\tPage " + (++pageCount) + "\n\n";
+            string pageString = "";
+            
+            int extraCounter = 0;
+            for (int i = 0; i < storyString.Count(); i++)
+            {
+
+
+
+                pageString += storyString[i] + " ";
+
+                if (pageString.Length >= 265)
+                {
+
+                    outputTextBox.Text += pageString + "\n\n";
+
+                    pageString = "";
+                    outputTextBox.Text += "\n\n\tPage " + (++pageCount) + "\n\n";
+
+                    extraCounter = (i + 1);
+                }
+
+
+
+                //while (pageString.Length <= 180)
+                //{
+                //    pageString += storyString[i];
+                //}
+
+
+            }
+
+            for (int i = 0; extraCounter < storyString.Count(); extraCounter++)
+            {
+
+
+                outputTextBox.Text += storyString[extraCounter] + " ";
+
+            }
+        }
+    }
+}
